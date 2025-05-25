b7de6ab923ad938aec641ea603443209884dc89d
tsartrooper <pratmane00007@gmail.com>
2025-05-25 19:36:13 +0530

    fcm

--- lib/consts/AppConts.dart
+++ lib/consts/AppConts.dart
@@ -1,6 +1,6 @@
-const GOOGLE_API_KEY = "AIzaSyCWRnC80dwhB3oe9xYI1mZD6vsSmJW_pWY";
-const PICOVOICE_API_KEY = "Xc45hBG4r0uay1MlFPjd4fDs+jzPHg8TjG0qpFFG0UgmV9UcQJPOxw==";
-const String TESTURL = "https://aabid.up.railway.app";
-const String MAINURL = "https://womensafety-production-c18e.up.railway.app";
-const String WEBSOCKETURL = "wss://womensafety-production-c18e.up.railway.app";
-const String PHONE_EMAIL = "QShwRW8OQQyFocYqysO4o3Eep63oxG7f";
+const GOOGLE_API_KEY = "";
+const PICOVOICE_API_KEY = "";
+const String TESTURL = "";
+const String MAINURL = "";
+const String WEBSOCKETURL = "";
+const String PHONE_EMAIL = "";
